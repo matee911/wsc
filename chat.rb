0876c659179a10afe59931ffb2f731e0dfd61574
matee <matee@matee.net>
2010-03-10 00:21:38 +0100

    prv messages

--- chat.rb
+++ chat.rb
@@ -17,11 +17,27 @@ class Action
     
     def connect(user, data, socket)
       @@users[user] = socket unless @@users.include? user
-      return "#{user} connected"
+      return @@users, "#{user} connected"
+    end
+    
+    def leave(socket)
+      user = @@users.index socket
+      @@users.delete user
+      return @@users, "%s disconnected" % [user]
     end
     
     def message(user, data, socket)
-      return format("%s: %s", user, data['body']) # "%s" % ['foo']
+      # prv only to one user
+      if data['body'].strip[0].chr == '^' # taak, wesołe miasteczko
+        rcpt_to = data['body'].strip.split.first[1..-1] # i tu
+        if @@users.member? rcpt_to
+          return {rcpt_to => @@users[rcpt_to], user => socket}, format("%s: %s", user, data['body'])
+        else
+          return {user => socket}, "Message to unknown recipient %s: %s" % [rcpt_to, data['body']]
+        end
+      end
+      # all users
+      return @@users, format("%s: %s", user, data['body']) # "%s" % ['foo']
     end
   end
 end
@@ -36,16 +52,20 @@ EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 1234) do |socket|
   }
 
   socket.onmessage  { |msg|
-    message = Action.parse_message msg, socket
+    users, message = Action.parse_message msg, socket
     unless message.nil?
-      Action.users.each do |user, ws|
+      users.each do |user, ws|
         ws.send "#{message}"
       end
     end
   }
-  socket.onclose { 
+  socket.onclose {
+    users, message = Action.leave socket
+    unless message.nil?
+      users.each do |user, ws|
+        ws.send "#{message}"
+      end
+    end
     puts "Connection closed"
-    # @sockets.delete socket
-    #socket.onmessage("Someone disconnected.")
   }
 end
