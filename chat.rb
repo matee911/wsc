ec71c3966d97dd4a50876653a48fec6c812c7ec4
matee <matee@matee.net>
2010-03-09 23:19:01 +0100

    changes

--- chat.rb
+++ chat.rb
@@ -1,23 +1,51 @@
-
 require 'rubygems'
 require 'em-websocket'
+require 'json'
 
-EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 1234, :debug => true) do |socket|
+class Action
+  @@users = {}
   
+  class << self
+    def users
+      return @@users
+    end
+    
+    def parse_message message, socket
+      user, data = JSON.parse! message
+      return Action.send(data['cmd'], user, data, socket)
+    end
+    
+    def connect(user, data, socket)
+      @@users[user] = socket unless @@users.include? user
+      return "#{user} connected"
+    end
+    
+    def message(user, data, socket)
+      return format("%s: %s", user, data['body']) # "%s" % ['foo']
+    end
+  end
+end
+
+EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 1234) do |socket|
+
   socket.onopen { 
     puts "New connection"
-    @sockets ||= []
-    @sockets << socket
+    puts socket.request.inspect
+    # @sockets ||= []
+    # @sockets << socket
   }
-  
+
   socket.onmessage  { |msg|
-    @sockets.each do |s|
-      puts s.inspect
-      s.send "#{msg}"
+    message = Action.parse_message msg, socket
+    unless message.nil?
+      Action.users.each do |user, ws|
+        ws.send "#{message}"
+      end
     end
   }
   socket.onclose { 
     puts "Connection closed"
-    @sockets.delete socket
+    # @sockets.delete socket
+    #socket.onmessage("Someone disconnected.")
   }
 end
